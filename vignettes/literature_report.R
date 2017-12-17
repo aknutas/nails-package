daa622016c2c0f702d9bc72e953dd0174da4330f
Antti Knutas <antti.knutas@lut.fi>
2017-12-17 00:11:55 +0000

    Literature report options set

---
+++ vignettes/literature_report.R
@@ -0,0 +1,4 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = FALSE,
+                      message = FALSE,
+                      warning = FALSE)
